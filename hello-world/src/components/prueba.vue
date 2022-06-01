8e586cbc3a7aea01eb57623d6113c91e385fbdd2
f_javier <fjmr.10messi@gmail.com>
2022-06-01 20:23:06 +0200

    message

---
+++ hello-world/src/components/prueba.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="prueba">
+        <h1>Esto es una prueba de que funciona, o eso creo</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'prueba',
+        props:{
+            msg: String
+        }
+    }
+</script>
+
+<style>
+    .prueba{
+        position: absolute;
+        width: 100%;
+        text-align: center;
+    }
+</style>
